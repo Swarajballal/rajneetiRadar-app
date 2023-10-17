8d954324f1bc4b3a9afa69e1d3007efa54be2105
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-10-17 16:14:43 +0530

    added trpc server and client code 17/10/23

--- packages/trpc/trpc-server/db.ts
+++ packages/trpc/trpc-server/db.ts
@@ -1,27 +1,35 @@
-import { PrismaClient, Role } from "database";  
+import { PrismaClient, Role, User } from "database";  
 export { Role } from "database";
+import { z } from "zod";
 
 const prismadb = new PrismaClient();
 
-type UserInput = {
-    email: string;
-    hashedPassword: string;
-    firstName?: string | null;
-    lastName?: string | null;
-    role?: Role;
-    emailVerified?: Date | null;
-    updatedAt: Date;
-    createdAt: Date;
-}
+const UserInput = z.object({
+    email: z.string().email(),
+    hashedPassword: z.string().max(8),
+    firstName: z.string().nullable(),
+    lastName: z.string().nullable(),
+});
+
+const UserCreationInput = z.object({
+    email: z.string().email(),
+    hashedPassword: z.string().max(8),
+    firstName: z.string().nullable(),
+    lastName: z.string().nullable(),
+    role: z.nativeEnum(Role),
+    emailVerified: z.date().nullable(),
+    updatedAt: z.date(),
+    createdAt: z.date(),
+});
 
 export const db = {
     user: {
         findMany: async () => prismadb.user.findMany(),
-        findById: async (id: string) => prismadb.user.findUnique({ where: { id } }),
-        create: async (data: UserInput) => {
+        findById: async (id: User['id']) => prismadb.user.findUnique({ where: { id } }),
+        findUnique: async (email: z.infer<typeof UserInput>['email']) => prismadb.user.findUnique({ where: { email } }),
+        create: async (data: z.infer<typeof UserCreationInput>) => {
             const user = await prismadb.user.create({ data });
             return user;
         },
     }
-    
 };
--- packages/trpc/trpc-server/index.ts
+++ packages/trpc/trpc-server/index.ts
@@ -2,7 +2,7 @@ import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import { z } from "zod";
 import { db , Role} from "./db";
 import { publicProcedure, router } from "./trpc";
- 
+
 const appRouter = router({
   userList: publicProcedure
     .query(async () => {
@@ -16,28 +16,58 @@ const appRouter = router({
       const user = await db.user.findById(input);
       return user;
     }),
-    userCreate: publicProcedure
+  userCreate: publicProcedure
     .input(z.object({
       email: z.string(),
       hashedPassword: z.string(),
       firstName: z.string().nullable(),
       lastName: z.string().nullable(),
-      role: z.nativeEnum(Role),
-      emailVerified: z.date().nullable(),
-      updatedAt: z.date(),
-      createdAt: z.date()
     }))
     .mutation(async (opts) => {
       const { input } = opts;
-      const user = await db.user.create(input);
+      const user = await db.user.create({
+        email: input.email,
+        hashedPassword: input.hashedPassword,
+        firstName: input.firstName,
+        lastName: input.lastName,
+        role: Role.USER,
+        emailVerified: new Date(),
+        updatedAt: new Date(),
+        createdAt: new Date(),
+      });
       return user;
     }),
+  signUp: publicProcedure
+    .input(z.object({
+      email: z.string(),
+      hashedPassword: z.string(),
+      firstName: z.string(),
+      lastName: z.string(),
+    }))
+    .mutation(async (opts) => {
+      const { input } = opts;
+      const existingUser = await db.user.findUnique(input.email); 
+      if(existingUser) {
+        throw new Error('User already exists');
+      }
+      const user = await db.user.create({
+        email: input.email,
+        hashedPassword: input.hashedPassword,
+        firstName: input.firstName,
+        lastName: input.lastName,
+        role: Role.USER,
+        emailVerified: new Date(),
+        updatedAt: new Date(),
+        createdAt: new Date(),
+      });
+      return user;
+    })
 });
- 
-export type AppRouter = typeof appRouter;
- 
+
 const server = createHTTPServer({
   router: appRouter,
 });
- 
-server.listen(3000);
+
+server.listen(8000);
+
+export type AppRouter = typeof appRouter;
